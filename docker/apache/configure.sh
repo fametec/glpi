d7b89bb2853addb428b93c24ff768b993f71ca5e
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2019-06-25 20:28:21 -0300

    Upgrade to 9.4.3

--- docker/apache/configure.sh
+++ docker/apache/configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# set -x
+set -x
 # 
 
 SetSubVersion () {
@@ -11,21 +11,31 @@ SetSubVersion () {
 }
 
 SetPermission () {
+
     chown -Rf apache:apache /var/www/html/glpi
+
 }
 
 GetCurrentVersion () {
+
     echo "{ `curl -s http://localhost/glpi/ajax/telemetry.php | grep -v code` }" | jq -r '.glpi.version'
+
 }
 
 Install () {
+
     echo "Download and install GLPI $VERSION ..."
     
     if [ -e /glpi-$VERSION.tgz ]; then
+
       tar -zxf /glpi-$VERSION.tgz -C /var/www/html/
+
     else
+
       curl --progress-bar -L https://github.com/glpi-project/glpi/releases/download/$VERSION/glpi-$VERSION.tgz | tar -zxf - -C /var/www/html/
+
     fi
+
     SetPermission	
 }
 
@@ -35,21 +45,31 @@ Upgrade () {
 
     if [ $? -ge 4 ]; then
       echo "Upgrade to $VERSION using bin/console..."
+
       /usr/bin/php /var/www/html/glpi/bin/console glpi:database:update --no-interaction
+
       /usr/bin/php /var/www/html/glpi/bin/console glpi:migration:myisam_to_innod --no-interaction
+
     else
+
       echo "Upgrade to $VERSION using cliupdate script..."
+
       /usr/bin/php /var/www/html/glpi/scripts/cliupdate.php
+
       /usr/bin/php /var/www/html/glpi/scripts/innodb_migration.php
+
     fi
 }
 
 
 RemoveInstall () {
+
     rm -rf /var/www/html/glpi/install/install.php;
+
 }
 
 ConfigDataBase () {
+
       {
         echo "<?php"; \
         echo "class DB extends DBmysql {"; \
@@ -61,6 +81,7 @@ ConfigDataBase () {
         echo "}"; \
         echo ; 
       } > /var/www/html/glpi/config/config_db.php
+
 }
 
 DeployDataBase () {
@@ -68,17 +89,21 @@ DeployDataBase () {
     SetSubVersion $VERSION 2
 
     if [ $? -ge 4 ]; then
+
       echo "Deploy DB using bin/console. Please wait..."
+
       /usr/bin/php /var/www/html/glpi/bin/console glpi:database:install \
 	--no-interaction \
-	--db-host=${MARIADB_HOST} \
-	--db-port=${MARIADB_PORT} \
+	--db-host=${MARIADB_HOST}:${MARIADB_PORT} \
 	--db-name=$MARIADB_DATABASE \
 	--db-user=$MARIADB_USER \
 	--db-password=$MARIADB_PASSWORD \
+	--force \
 	--default-language=$GLPI_LANG
     else 
+      
       echo "Deploy DB using cliinstall.php. Please wait..." 
+      
       /usr/bin/php /var/www/html/glpi/scripts/cliinstall.php \
         --host=${MARIADB_HOST} \
         --hostport=${MARIADB_PORT} \
@@ -86,17 +111,23 @@ DeployDataBase () {
         --user=$MARIADB_USER \
         --pass=$MARIADB_PASSWORD \
         --lang=$GLPI_LANG 
-   fi
+   
+    fi
+
 }
 
 if [ ! -d /var/www/html/glpi/ ]; then
+
     echo "Directory not found, go to install..." 
 
     Install 
 
     if [ $? -ne 0 ]; then
+
       echo "fail"
+
       exit 2
+
     fi
 
 fi
