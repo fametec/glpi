59a94d6ca2fd86bfdf2fc8ce386584ebedb64b60
Eduardo Fraga <eduardo@fametec.com.br>
2019-08-24 12:54:28 -0300

    remove old plugins

--- docker/apache/plugins.sh
+++ docker/apache/plugins.sh
@@ -9,7 +9,29 @@ SetPermission () {
 
 }
 
+RemoveOldPlugin() {
+
+	if [ -z $1 ]
+	then
+		return "Use: $0 directory"
+	fi
+
+	if [ ! -d /var/www/html/glpi/$1 ]
+	then
+
+		echo "Directory not found."
+
+	else
+
+		rm -rf /var/www/html/glpi/$1
+
+	fi
+
+}
+
 PluginModifications() {
+
+	RemoveOldPlugin Mod
 
 	curl --progress-bar -L "https://github.com/stdonato/glpi-modifications/archive/1.4.0.tar.gz" | tar -zxf - -C /var/www/html/glpi/plugins/
 
@@ -18,13 +40,19 @@ PluginModifications() {
 }
 
 
+
+
 PluginTelegramBot() {
+
+	RemoveOldPlugin telegrambot
 
 	curl --progress-bar -L "https://github.com/pluginsGLPI/telegrambot/releases/download/2.0.0/glpi-telegrambot-2.0.0.tar.bz2" | tar -jxf - -C /var/www/html/glpi/plugins/
 
 }
 
 PluginPDF() {
+
+	RemoveOldPlugin pdf
 
 	curl --progress-bar -L "https://forge.glpi-project.org/attachments/download/2293/glpi-pdf-1.6.0.tar.gz" | tar -zxf - -C /var/www/html/glpi/plugins/
 
@@ -33,6 +61,8 @@ PluginPDF() {
 
 PluginOCS() {
 
+	RemoveOldPlugin ocsinventoryng
+
     curl --progress-bar -L "https://github.com/pluginsGLPI/ocsinventoryng/releases/download/1.6.0/glpi-ocsinventoryng-1.6.0.tar.gz" | tar -zxf - -C /var/www/html/glpi/plugins/
 
 }
@@ -40,11 +70,15 @@ PluginOCS() {
 
 PluginDataInjection() {
 
+	RemoveOldPlugin datainjection
+
     curl --progress-bar -L "https://github.com/pluginsGLPI/datainjection/releases/download/2.7.0/glpi-datainjection-2.7.0.tar.bz2" | tar -jxf - -C /var/www/html/glpi/plugins/
 
 }
 
 PluginFields() {
+
+	RemoveOldPlugin fields
 
 	curl --progress-bar -L "https://github.com/pluginsGLPI/fields/releases/download/1.10.1/glpi-fields-1.10.1.tar.bz2" | tar -jxf - -C /var/www/html/glpi/plugins/
 
@@ -53,7 +87,9 @@ PluginFields() {
 
 PluginTasklists() {
 
-	curl --progress-bar -L "https://github.com/InfotelGLPI/tasklists/releases/download/1.5.0/glpi-tasklists.1.5.0.tar.gz" | tar -jxf - -C /var/www/html/glpi/plugins/ 
+	RemoveOldPlugin tasklists
+	
+	curl --progress-bar -L "https://github.com/InfotelGLPI/tasklists/releases/download/1.5.0/glpi-tasklists.1.5.0.tar.gz" | tar -zxf - -C /var/www/html/glpi/plugins/ 
 
 }
 
