2f23b8cf0008daf50cf0212791871d2e15fb6c6c
Eduardo Fraga <eduardo@eftech.com.br>
2020-03-14 10:46:12 -0300

    update file 01-glpi-empty.sql

--- docker/mariadb/src/01-glpi-empty.sql
+++ docker/mariadb/src/01-glpi-empty.sql
@@ -28,12 +28,6 @@
 #  * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
 #  * ---------------------------------------------------------------------
 # */
-
-CREATE DATABASE IF NOT EXISTS glpi character set utf8;
-
-USE glpi; 
-
-
 #GLPI Dump database on 2016-02-08 16:57
 
 ### Dump table glpi_alerts
