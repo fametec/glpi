95397eca424cddeb4e776e387d5214c5d07d5288
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2021-09-16 10:39:42 -0300

    Update glpi-entrypoint.sh

--- docker/apache/scripts/glpi-entrypoint.sh
+++ docker/apache/scripts/glpi-entrypoint.sh
@@ -17,6 +17,34 @@ ConfigDataBase () {
 }
 
 
+VerifyDir () {
+
+  DIR="/var/www/html/files/_cron
+  /var/www/html/files/_dumps
+  /var/www/html/files/_graphs
+  /var/www/html/files/_lock
+  /var/www/html/files/_pictures
+  /var/www/html/files/_plugins
+  /var/www/html/files/_rss
+  /var/www/html/files/_tmp
+  /var/www/html/files/_uploads
+  /var/www/html/files/_cache
+  /var/www/html/files/_sessions
+  /var/www/html/files/_locales"
+
+  for i in $DIR
+  do 
+    if [ ! -d $i ]
+    then
+      echo -n "Creating $i dir... " 
+      mkdir -p $i
+      chown -R apache:apache $i
+      echo "done"
+    fi
+}
+
+
+
 ConfigDataBase
 
 httpd -D FOREGROUND
